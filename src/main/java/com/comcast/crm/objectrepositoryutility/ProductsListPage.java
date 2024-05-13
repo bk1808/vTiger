94161ea0bdc10dea17d7bbe69790c3e868af2cac
Yashoda <Yashoda@LAPTOP-SMJ68QD9>
2024-05-13 23:04:35 +0530

    committed by yashodha

--- src/main/java/com/comcast/crm/objectrepositoryutility/ProductsListPage.java
+++ src/main/java/com/comcast/crm/objectrepositoryutility/ProductsListPage.java
@@ -8,32 +8,14 @@ import org.openqa.selenium.support.PageFactory;
 
 public class ProductsListPage {
 
-	@FindBy (xpath = "//img[@alt='Create Product...']")
-	private WebElement ele1;
-	
+		   
+	    //Yashoda
+		@FindBy (id ="bas_searchfield")
+		private WebElement ele2;
 
-	@FindBy (xpath = "//input[@class='txtBox']")
-	private WebElement ele2;
-
-	@FindBy (xpath = "//input[@name='submit']")
-	private WebElement ele3;
-
-
-	public WebElement getCreateProdImage() {
-		return ele1;
-	}
+		public WebElement getEle2() {
+			return ele2;
 		
-	public WebElement getSearchFieldEdt() {
-		return ele2;
-	}
-	
-	public WebElement getSearchNowBtn() {
-		return ele3;
-
-	}
-
-	public ProductsListPage(WebDriver driver) {
-		PageFactory.initElements(driver, this);
 	}
 	
 }
