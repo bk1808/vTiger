b1f639af98e37bd217e07d8fbac7264ece53a40a
bk1808 <praveen.bk1808@gmail.com>
2024-05-13 21:22:14 +0530

    committed by Steve

--- src/main/java/com/comcast/crm/objectrepositoryutility/ProductsListPage.java
+++ src/main/java/com/comcast/crm/objectrepositoryutility/ProductsListPage.java
@@ -5,13 +5,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-/**
- * This POM class represents the web elements of Products List page on CRM application
- * 
- * @author Praveen
- * 
- *  13-May-2024
- */
+
 public class ProductsListPage {
 
 	@FindBy (xpath = "//img[@alt='Create Product...']")
@@ -32,8 +26,6 @@ public class ProductsListPage {
 	public WebElement getSearchFieldEdt() {
 		return ele2;
 	}
-		
-	//step-1
 	
 	public WebElement getSearchNowBtn() {
 		return ele3;
