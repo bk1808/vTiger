63671a088dba87ce4fb81f4341aac71acfa4302c
Tushar Gavare <Tushar Gavare@TUSHAR>
2024-05-14 10:59:31 +0530

    committed by tushar

--- src/main/java/com/comcast/crm/objectrepositoryutility/ProductsListPage.java
+++ src/main/java/com/comcast/crm/objectrepositoryutility/ProductsListPage.java
@@ -8,32 +8,6 @@ import org.openqa.selenium.support.PageFactory;
 
 public class ProductsListPage {
 
-	@FindBy (xpath = "//img[@alt='Create Product...']")
-	private WebElement ele1;
-	
-
-	@FindBy (xpath = "//input[@class='txtBox']")
-	private WebElement ele2;
-
-	@FindBy (xpath = "//input[@name='submit']")
-	private WebElement ele3;
-
-
-	public WebElement getCreateProdImage() {
-		return ele1;
-	}
-		
-	public WebElement getSearchFieldEdt() {
-		return ele2;
-	}
-	
-	public WebElement getSearchNowBtn() {
-		return ele3;
-
-	}
-
-	public ProductsListPage(WebDriver driver) {
-		PageFactory.initElements(driver, this);
-	}
+	//commit by tushar
 	
 }
